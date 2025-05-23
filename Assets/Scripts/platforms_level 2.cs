57f757e57d9dd88511fd3174c883039c081a7a4a
DobJalo <marugrig@gmail.com>
2025-05-23 19:35:44 +0300

    I added more platforms

--- Assets/Scripts/platforms_level 2.cs
+++ Assets/Scripts/platforms_level 2.cs
@@ -12,63 +12,37 @@ public class platforms_level2 : MonoBehaviour
     public GameObject pl8;
     public GameObject pl9;
     public GameObject pl10;
+    public GameObject pl11;
+    public GameObject pl12;
+    public GameObject pl13;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            if(this.gameObject == pl1)
+            if(this.gameObject == pl9)
+            {
+                other.gameObject.transform.position = new Vector3(pl4.transform.position.x, pl4.transform.position.y, pl4.transform.position.z);
+            }
+            if (this.gameObject == pl8)
+            {
+                other.gameObject.transform.position = new Vector3(pl6.transform.position.x, pl6.transform.position.y, pl6.transform.position.z);
+            }
+            if (this.gameObject == pl5)
             {
                 other.gameObject.transform.position = new Vector3(pl3.transform.position.x, pl3.transform.position.y, pl3.transform.position.z);
             }
             if (this.gameObject == pl2)
             {
-                other.gameObject.transform.position = new Vector3(pl4.transform.position.x, pl4.transform.position.y, pl4.transform.position.z);
-            }
-            if (this.gameObject == pl3)
-            {
-                //other.gameObject.transform.position = new Vector3(pl5.transform.position.x, pl5.transform.position.y, pl5.transform.position.z);
-            }
-            if (this.gameObject == pl4)
-            {
-                //other.gameObject.transform.position = new Vector3(pl2.transform.position.x, pl2.transform.position.y, pl2.transform.position.z);
-            }
-            if (this.gameObject == pl5)
-            {
-                other.gameObject.transform.position = new Vector3(pl4.transform.position.x, pl4.transform.position.y, pl4.transform.position.z);
-            }
-            if (this.gameObject == pl6)
-            {
-                //other.gameObject.transform.position = new Vector3(pl3.transform.position.x, pl3.transform.position.y, pl3.transform.position.z);
-            }
-            if (this.gameObject == pl7)
-            {
-                //other.gameObject.transform.position = new Vector3(pl4.transform.position.x, pl4.transform.position.y, pl4.transform.position.z);
-            }
-            if (this.gameObject == pl8)
-            {
                 other.gameObject.transform.position = new Vector3(pl7.transform.position.x, pl7.transform.position.y, pl7.transform.position.z);
             }
-            if (this.gameObject == pl9)
+            if (this.gameObject == pl11)
             {
-                other.gameObject.transform.position = new Vector3(pl6.transform.position.x, pl6.transform.position.y, pl6.transform.position.z);
+                other.gameObject.transform.position = new Vector3(pl10.transform.position.x, pl10.transform.position.y, pl10.transform.position.z);
             }
-            if (this.gameObject == pl10)
-            {
-                //other.gameObject.transform.position = new Vector3(pl4.transform.position.x, pl4.transform.position.y, pl4.transform.position.z);
-            }
+
         }
     }
 }
 
-//1 -> 3
-//2 -> 4
-//3
-//4
-//5 -> 4
-//6 
-//7
-//8 -> 7
-//9 -> 6
-//10
