f8b084edba137ec890eac7484ef47bd990ae81c5
DobJalo <marugrig@gmail.com>
2025-05-30 15:33:55 +0300

    Platforms fix

--- Assets/Scripts/platforms_level 2.cs
+++ Assets/Scripts/platforms_level 2.cs
@@ -2,44 +2,41 @@ using UnityEngine;
 
 public class platforms_level2 : MonoBehaviour
 {
-    public GameObject pl1;
-    public GameObject pl2;
-    public GameObject pl3;
-    public GameObject pl4;
-    public GameObject pl5;
-    public GameObject pl6;
-    public GameObject pl7;
-    public GameObject pl8;
-    public GameObject pl9;
-    public GameObject pl10;
-    public GameObject pl11;
-    public GameObject pl12;
-    public GameObject pl13;
+    public GameObject pl1_1;
+    public GameObject pl1_2;
+
+    public GameObject pl2_1;
+    public GameObject pl2_2;
+
+    public GameObject pl3_1;
+    public GameObject pl3_2;
+
+    public GameObject pl4_1;
+    public GameObject pl4_2;
+
+    private int dist = 2;
+
 
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            if(this.gameObject == pl9)
+            if(this.gameObject == pl1_1)
             {
-                other.gameObject.transform.position = new Vector3(pl4.transform.position.x, pl4.transform.position.y, pl4.transform.position.z);
+                other.gameObject.transform.position = new Vector3(pl1_2.transform.position.x, pl1_2.transform.position.y + dist, pl1_2.transform.position.z);
             }
-            if (this.gameObject == pl8)
+            if (this.gameObject == pl2_1)
             {
-                other.gameObject.transform.position = new Vector3(pl6.transform.position.x, pl6.transform.position.y, pl6.transform.position.z);
+                other.gameObject.transform.position = new Vector3(pl2_2.transform.position.x, pl2_2.transform.position.y + dist, pl2_2.transform.position.z);
             }
-            if (this.gameObject == pl5)
+            if (this.gameObject == pl3_1)
             {
-                other.gameObject.transform.position = new Vector3(pl3.transform.position.x, pl3.transform.position.y, pl3.transform.position.z);
+                other.gameObject.transform.position = new Vector3(pl3_2.transform.position.x, pl3_2.transform.position.y + dist, pl3_2.transform.position.z);
             }
-            if (this.gameObject == pl2)
+            if (this.gameObject == pl4_1)
             {
-                other.gameObject.transform.position = new Vector3(pl7.transform.position.x, pl7.transform.position.y, pl7.transform.position.z);
-            }
-            if (this.gameObject == pl11)
-            {
-                other.gameObject.transform.position = new Vector3(pl10.transform.position.x, pl10.transform.position.y, pl10.transform.position.z);
+                other.gameObject.transform.position = new Vector3(pl4_2.transform.position.x, pl4_2.transform.position.y + dist, pl4_2.transform.position.z);
             }
 
         }
